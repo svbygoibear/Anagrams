79b257b972099c7641d256927604357b117debd5
simoneslatnpou <svbyogibear@gmail.com>
2016-10-12 15:06:14 +0200

    Added an easy file reader.

---
+++ Anagrams/Helpers/FileHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Anagrams.Helpers {
+    public static class FileHandler {
+        /// <summary>
+        /// Reads a file from a path and returns each line in a list of string.
+        /// </summary>
+        /// <param name="filePath">Path of the file to be read</param>
+        /// <returns></returns>
+        public static List<string> readFile(this string filePath) {
+            if (File.Exists(filePath))
+                return File.ReadAllLines(filePath).ToList();
+            else
+                return new List<string>();
+        }
+    }
+}
