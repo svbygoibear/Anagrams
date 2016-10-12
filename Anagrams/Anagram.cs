eff68e68ed26f7f40060d43ba16c7c71d0ef1a26
simoneslatnpou <svbyogibear@gmail.com>
2016-10-12 15:09:04 +0200

    Added a method which returns all the matched anagrams from a list of string.

--- Anagrams/Anagram.cs
+++ Anagrams/Anagram.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Anagrams.Helpers;
 
-namespace Anagrams
-{
-    public class Anagram
-    {
+namespace Anagrams {
+    public static class Anagram {
+        /// <summary>
+        /// Returns all the anagrams form a list of string given an input word.
+        /// </summary>
+        /// <param name="anagramBase">List of string to search.</param>
+        /// <param name="word">Original word</param>
+        /// <returns>List of all the matches,</returns>
+        public static List<string> getAllAnagrams(List<string> anagramBase, string word) {
+            var matches = new List<string>();
+            anagramBase.ForEach(anagram => {
+                if (word.isAnagram(anagram))
+                    matches.Add(anagram);
+            });
+            return matches;
+        }
     }
 }
