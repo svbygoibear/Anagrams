210083e846224a075b1b8f288cc618bc89ea93f7
simoneslatnpou <svbyogibear@gmail.com>
2016-10-12 15:20:36 +0200

    Fixed bugs and finished up unit tests.

---
+++ AnagramUnitTest/AnagramTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anagrams;
+using System.Collections.Generic;
+
+namespace AnagramUnitTest {
+    [TestClass]
+    public class AnagramTest {
+        [TestMethod]
+        public void GetAnagrams() {
+            var word = "horse";
+            var words = new List<string> { "shore", "mist", "heros", "horse" };
+            var matches = Anagram.getAllAnagrams(words, word);
+
+            Assert.AreEqual(matches.Count, 3);
+        }
+    }
+}
